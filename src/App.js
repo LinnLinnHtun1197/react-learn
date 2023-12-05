afd5f88f24007d6e20350a3ff0b47bf21e12e3ab
linnlinnhtun96 <linn@era.com.sg>
2023-12-05 11:00:43 +0800

    updated class to functional component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,112 @@
-import React from "react";
+import React, { createRef, useState } from "react";
+// import Item from './Item';
 
-class Item extends React.Component {
-  render() {
-    return <li>
-      {this.props.name} 
-       ${this.props.price}
-      </li>
-  }
-}
+// class Item extends React.Component {
+//   render() {
+//     return <li>
+//       {this.props.name} 
+//        ${this.props.price}
+//       </li>
+//   }
+// }
 
-class AddForm extends React.Component {
-  nameRef = React.createRef();
-  priceRef = React.createRef();
+// class AddForm extends React.Component {
+//   nameRef = React.createRef();
+//   priceRef = React.createRef();
 
-  add = () => {
-    let name = this.nameRef.current.value;
-    let price = this.priceRef.current.value;
+//   add = () => {
+//     let name = this.nameRef.current.value;
+//     let price = this.priceRef.current.value;
 
-    this.props.add(name, price);
-  }
+//     this.props.add(name, price);
+//   }
 
-  render() {
-    return (
-      <div>
-        <input type="text" ref={this.nameRef} /><br />
-        <input type="text" ref={this.priceRef} /><br />
-        <button onClick={this.add}>Add</button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <input type="text" ref={this.nameRef} /><br />
+//         <input type="text" ref={this.priceRef} /><br />
+//         <button onClick={this.add}>Add</button>
+//       </div>
+//     );
+//   }
+// }
 
-class App extends React.Component {
-  state = {
-    items: [
-        { id: 1, name: 'Apple', price: 0.99 },
-        { id: 2, name: 'Orange', price: 0.89 },
-      ]
-  };
+// class App extends React.Component {
+//   state = {
+//     items: [
+//         { id: 1, name: 'Apple', price: 0.99 },
+//         { id: 2, name: 'Orange', price: 0.89 },
+//       ]
+//   };
 
-    add = (name, price) => {
-      let id = this.state.items.length + 1;
+//     add = (name, price) => {
+//       let id = this.state.items.length + 1;
 
-      this.setState({
-        items: [
-          ...this.state.items,
-          { id, name, price }
-        ]
-      });
-    }
+//       this.setState({
+//         items: [
+//           ...this.state.items,
+//           { id, name, price }
+//         ]
+//       });
+//     }
     
-    render() {
-      return (
-        <div>
-          <h1>Hello React</h1>
-          <ul>
-            {this.state.items.map(i => {
-              return (
-                <Item
-                key={i.id}
-                name={i.name}
-                price={i.price}
-                />
-              )
-            })}
-          </ul>
-          <AddForm add={this.add} />
-        </div>
-      )
-    }
+//     render() {
+//       return (
+//         <div>
+//           <h1>Hello React</h1>
+//           <ul>
+//             {this.state.items.map(i => {
+//               return (
+//                 <Item
+//                 key={i.id}
+//                 name={i.name}
+//                 price={i.price}
+//                 />
+//               )
+//             })}
+//           </ul>
+//           <AddForm add={this.add} />
+//         </div>
+//       )
+//     }
+// }
+
+const Item = ({name, price}) => (
+  <li>{name}, ${price}</li>
+);
+
+const App = props => {
+  let [items, setItems] = useState([
+    {id: 1, name: 'Apple', price: 0.99},
+    {id: 2, name: 'Orange', price: 0.89}
+
+  ]);
+
+  let nameRef = createRef();
+  let priceRef = createRef();
+
+  let add = () => {
+    let id = items.length + 1;
+    let name = nameRef.current.value;
+    let price = priceRef.current.value;
+
+    setItems([
+      ...items,
+      {id, name, price}
+    ]);
+  }
+
+  return (
+    <div>
+      <ul>
+        {items.map(i => <Item key={i.id} name={i.name} price={i.price} />)}
+      </ul>
+      <input type="text" ref={nameRef} /> <br />
+      <input type="text" ref={priceRef} /> <br />
+      <button onClick={add}>Add</button>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Item.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Item = props => {
+    return (
+        <li>
+            {props.name},
+            $ {props.price}
+        </li>
+    )
+}
+
+export default Item;
